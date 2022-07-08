12e026d8d3665b115d78e678e23fcbfa0c665b3f
Admin <Admin@DESKTOP-R9OBK3V>
2022-07-08 22:19:58 +0530

    Hellow World

---
+++ src/com/tns/assignment/Hellow.java
@@ -0,0 +1,11 @@
+package com.tns.assignment;
+
+public class Hellow {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("Jai Ganesh");
+
+	}
+
+}
