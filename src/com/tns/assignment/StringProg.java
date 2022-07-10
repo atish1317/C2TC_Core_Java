a98bc823ad6b82b0ad07368f5a8497d81cfeb7e6
Admin <Admin@DESKTOP-R9OBK3V>
2022-07-10 20:07:02 +0530

    5th assignment

---
+++ src/com/tns/assignment/StringProg.java
@@ -0,0 +1,11 @@
+package com.tns.assignment;
+
+public class StringProg {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String atish = new String("M20 batch student");
+		System.out.print(atish);
+	}
+
+}
