522c5f4494369891115178729162489baf10e3be
Admin <Admin@DESKTOP-R9OBK3V>
2022-07-13 22:42:45 +0530

    TRyAndCatchBlock

---
+++ src/com/tns/assignment/TryAndCatchBlock.java
@@ -0,0 +1,21 @@
+package com.tns.assignment;
+
+public class TryAndCatchBlock {
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		try
+		{
+			System.out.println(5);
+			System.out.println(6/0);
+			System.out.println(7);
+		}
+		catch(ArithmeticException e)
+		{
+			System.out.println(e);
+		}
+
+	}
+
+}
