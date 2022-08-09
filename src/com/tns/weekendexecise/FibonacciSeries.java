ed97cf5a8165daa21076ee40ba790ae44d4549a2
Admin <Admin@DESKTOP-R9OBK3V>
2022-08-09 23:15:42 +0530

    fb

---
+++ src/com/tns/weekendexecise/FibonacciSeries.java
@@ -0,0 +1,21 @@
+package com.tns.weekendexecise;
+
+public class FibonacciSeries {
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		//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
+		
+		int a=0, b=1;
+		System.out.print(a+" "+b);
+		int c;
+		for (int i=1;i<=10;i++);
+		{c=a+b;
+		System.out.print(" "+c);
+		a=b;
+		b=c;
+		}
+	}
+
+}
